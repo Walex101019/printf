66193daaa4299f9ffc87ab621f588127198b5896
Walex101019 <ibietanwasiu94@gmail.com>
2023-08-25 19:51:14 -0700

    handles custom conversion

--- _print.c
+++ _print.c
@@ -110,3 +110,24 @@ void print_number(int num)
 		reversed /= 10;
 	}
 }
+void print_binary(unsigned int num)
+{
+	int binary[32];
+        int i = 0;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (num > 0)
+	{
+		binary[i] = num % 2;
+		num /= 2;
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(binary[i] + '0');
+	}
+}
--- main.h
+++ main.h
@@ -6,5 +6,8 @@ int _putchar(char c);
 void print_char(va_list args, int *printed_chars);
 void print_string(va_list args, int *printed_chars);
 void print_integer(va_list args, int *printed_chars);
+int get_num_length(int num);
+void print_number(int num);
+void print_binary(unsigned int num);
 
 #endif /* MAIN_H */
