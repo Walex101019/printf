4d4baeb757333e3539d990e1e787e2ffa18a9b00
Walex101019 <ibietanwasiu94@gmail.com>
2023-08-25 18:00:51 -0700

    handles conversion specifiers

---
+++ printf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * _printf - produces output according to a format
+ * @format: a character string composed of zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0;
+    int count = 0;
+
+    va_start(args, format);
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    count += _putchar(va_arg(args, int));
+                    break;
+                case 's':
+                    count += print_string(va_arg(args, char *));
+                    break;
+                case '%':
+                    count += _putchar('%');
+                    break;
+                case 'd':
+                case 'i':
+                    count += print_int(va_arg(args, int));
+                    break;
+                default:
+                    count += _putchar('%');
+                    count += _putchar(format[i]);
+                    break;
+            }
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
+    va_end(args);
+    return (count);
+}
